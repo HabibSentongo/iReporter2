a60beb8f0fe1102e9585e2853cc0e14635df4c26
HabibSentongo <habibsentongo@gmail.com>
2019-01-24 04:38:54 +0300

    refactor methods to improve on maintainability [Delivers #163443278]

--- app/models/incident_model.py
+++ app/models/incident_model.py
@@ -1,7 +1,13 @@
-from datetime import datetime
-"""Imports datetime to give us current time when creating an incident"""
-
-
-class Incident:
-
-    pass
+class Static_strings:
+    all_records = "SELECT * FROM {};"
+    error_empty = 'No records yet!'
+    error_missing = 'No such record'
+    error_bad_data = 'Provide correct incident details'
+    error_no_id = 'We can\'t identify you, Provide your ID'
+    msg_deleted = 'intervention record has been deleted'
+    msg_updated = 'Updated red-flag record location'
+    selector = 'SELECT {} FROM {} WHERE incident_id = {};'
+    creator = "INSERT INTO {}(comment,created_by, location, images, videos)\
+     VALUES ('{}',{},'{}','{}','{}') RETURNING incident_id;"
+    updater = "UPDATE {} SET {} = '{}' WHERE incident_id = {} RETURNING incident_id;"
+    deleter = "DELETE FROM {} WHERE incident_id = {} RETURNING incident_id;"

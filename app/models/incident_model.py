44b60fd5a06bca2fc7ffb44fba7ed432e2208618
HabibSentongo <habibsentongo@gmail.com>
2019-01-25 08:47:34 +0300

    add tests for all endpoints and implement DLFA feedback [Delivers #163474429]

--- app/models/incident_model.py
+++ app/models/incident_model.py
@@ -4,8 +4,8 @@ class Static_strings:
     error_missing = 'No such record'
     error_bad_data = 'Provide correct incident details'
     error_no_id = 'We can\'t identify you, Provide your ID'
-    msg_deleted = 'intervention record has been deleted'
-    msg_updated = 'Updated red-flag record location'
+    msg_deleted = 'Record has been deleted'
+    msg_updated = 'Updated record'
     selector = 'SELECT {} FROM {} WHERE incident_id = {};'
     creator = "INSERT INTO {}(comment,created_by, location, images, videos)\
      VALUES ('{}',{},'{}','{}','{}') RETURNING incident_id;"
--- config.py
+++ config.py
@@ -1,31 +1,36 @@
-import os
+# import os
 
-class GeneralConfig:
-    DEBUG = False
-    TESTING = False
-    DB_URI = os.getenv('DATABASE_URL')
+# class GeneralConfig:
+#     DEBUG = False
+#     TESTING = False
+#     DB_URI = os.getenv('DATABASE_URL')
     
 
-class DevConfig(GeneralConfig):
-    DEBUG = True
-    ENV = 'dev_env'
-    DATABASE = 'iReporter_db'
-    TESTING = False
+# class DevConfig(GeneralConfig):
+#     DEBUG = True
+#     ENV = 'dev_env'
+#     DATABASE = 'iReporter_db'
+#     TESTING = False
 
 
-class TestConfig(GeneralConfig):
-    DEBUG = True
-    ENV = 'test_env'
-    DATABASE = 'test_iReporter_db'
-    TESTING = False
+# class TestConfig(GeneralConfig):
+#     DEBUG = True
+#     ENV = 'test_env'
+#     DATABASE = 'test_iReporter_db'
+#     TESTING = True
 
-class DeployConfig(GeneralConfig):
-    DEBUG = False
+# class DeployConfig(GeneralConfig):
+#     DEBUG = False
+#     ENV = 'deploy_env'
+#     DATABASE_URI = 'postgres://nixbotuppxfnht:eb17041db3517c38f2479ea2c2f195ab65cc19718ceff3a903244f064e6b3507@ec2-54-227-246-152.compute-1.amazonaws.com:5432/derui7kb7ke7dv'
+#     DATABASE = 'derui7kb7ke7dv'
+#     HOST = 'postgres://nixbotuppxfnht:eb17041db3517c38f2479ea2c2f195ab65cc19718ceff3a903244f064e6b3507@ec2-54-227-246-152.compute-1.amazonaws.com:5432/derui7kb7ke7dv'
+#     USER = 'nixbotuppxfnht'
+#     PASSWORD = 'eb17041db3517c38f2479ea2c2f195ab65cc19718ceff3a903244f064e6b3507'
+#     TESTING = False
 
-
-app_config_dict = {
-    'deploy':DeployConfig,
-    'test':TestConfig,
-    'dev':DevConfig
-
-}
+# app_config_dict = {
+#     'deploy':DeployConfig,
+#     'test':TestConfig,
+#     'dev':DevConfig
+# }
